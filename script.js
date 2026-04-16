4d46ca6ddd9cfe18c333b19fd113803511ea8734
alexandre2436 <alexandrelimal237@gmail.com>
2026-04-16 17:42:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,114 @@
+const result = document.querySelector(".result");
+const buttons = document.querySelectorAll(".buttons button");
 
-function activaLetra(eLemento){
-    const arrTexto = elemento.innerHTML.split('');
-    elemento.innerHTML = '';
-    arrTexto.forEach((Letra, i)=>{
-        setTimeout(()=>{
-            eLemento.innerHTML += Letra;
-        }, 75 * i);
-    });
+let currentNumber = "";
+let firstOperand = null;
+let operator = null;
+let restart = false;
+
+function updateResult(originClear = false) {
+  result.innerText = originClear ? 0 : currentNumber.replace(".", ",");
 }
 
+function addDigit(digit) {
+  if (digit === "," && (currentNumber.includes(",") || !currentNumber)) return;
 
-const titulo = document.querySelector('.digitando');
-activaLetra(titulo);
+  if (restart) {
+    currentNumber = digit;
+    restart = false;
+  } else {
+    currentNumber += digit;
+  }
+
+  updateResult();
+}
+
+function setOperator(newOperator) {
+  if (currentNumber) {
+    calculate();
+
+    firstOperand = parseFloat(currentNumber.replace(",", "."));
+    currentNumber = "";
+  }
+
+  operator = newOperator;
+}
+
+function calculate() {
+  if (operator === null || firstOperand === null) return;
+  let secondOperand = parseFloat(currentNumber.replace(",", "."));
+  let resultValue;
+
+  switch (operator) {
+    case "+":
+      resultValue = firstOperand + secondOperand;
+      break;
+    case "-":
+      resultValue = firstOperand - secondOperand;
+      break;
+    case "×":
+      resultValue = firstOperand * secondOperand;
+      break;
+    case "÷":
+      resultValue = firstOperand / secondOperand;
+      break;
+    default:
+      return;
+  }
+
+  if (resultValue.toString().split(".")[1]?.length > 5) {
+    currentNumber = parseFloat(resultValue.toFixed(5)).toString();
+  } else {
+    currentNumber = resultValue.toString();
+  }
+
+  operator = null;
+  firstOperand = null;
+  restart = true;
+  percentageValue = null;
+  updateResult();
+}
+
+function clearCalculator() {
+  currentNumber = "";
+  firstOperand = null;
+  operator = null;
+  updateResult(true);
+}
+
+function setPercentage() {
+  let result = parseFloat(currentNumber) / 100;
+
+  if (["+", "-"].includes(operator)) {
+    result = result * (firstOperand || 1);
+  }
+
+  if (result.toString().split(".")[1]?.length > 5) {
+    result = result.toFixed(5).toString();
+  }
+
+  currentNumber = result.toString();
+  updateResult();
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const buttonText = button.innerText;
+    if (/^[0-9,]+$/.test(buttonText)) {
+      addDigit(buttonText);
+    } else if (["+", "-", "×", "÷"].includes(buttonText)) {
+      setOperator(buttonText);
+    } else if (buttonText === "=") {
+      calculate();
+    } else if (buttonText === "C") {
+      clearCalculator();
+    } else if (buttonText === "±") {
+      currentNumber = (
+        parseFloat(currentNumber || firstOperand) * -1
+      ).toString();
+      updateResult();
+    } else if (buttonText === "%") {
+      setPercentage();
+    }
+  });
+});
