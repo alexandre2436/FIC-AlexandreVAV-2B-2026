7e83e1c86e3ea27b8762f1bfdc34a9a48af088a7
alexandre2436 <alexandrelimal237@gmail.com>
2026-05-13 11:37:52 -0300

    Add initial implementation of adicionar function

---
+++ script2.js
@@ -0,0 +1,9 @@
+let listaPessoas = "Ana-15,João-28,Carlos-17,Maria-50"
+let Nomes = listaPessoas.split(",")
+let pessoas = [];
+
+
+function adicionar() {
+    let nome = document.getElementById("nome").value;
+    let idade = Number(document.getElementById("idade")).value;
+}
