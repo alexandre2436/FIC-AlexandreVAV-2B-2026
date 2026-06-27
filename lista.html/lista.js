e83cc2b67eeaae660fcfa2f189331f23d9bb1b6e
alexandre2436 <alexandrelimal237@gmail.com>
2026-06-27 14:29:33 -0300

    Create lista.html with voting list functionality

---
+++ lista.html/lista.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista</title>
+</head>
+<body>
+
+    <h1>Lista de votação</h1>
+    <input type="text" id="nome" placeholder="digite seu nome">
+    <input type="number" id="idade" placeholder="digite sua idade">
+
+    <button onclick="adicionar()">adicionar</button>
+
+    <h2> resultado:</h2>
+    <ul id="lista"></ul>
+
+    <h2 id="resultado"></h2>
+
+    <script src="script1.js"></script>
+    
+</body>
+</html>
